f6b501a848217071828bf156b552b3334fca51b4
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-03 09:30:45 +0530

    03062020: Day 3

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,12 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+    let result = [];
+    let great = inputArray[0].length;
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i].length >= great) {
+            result.push(inputArray[i])
+        }
+    }
+    return result;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
