7611d71b6cabfa5a7ab004eeea8dae4d7f87b085
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-04 22:07:03 +0530

    04062020: Day 4

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,16 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-
+    let strike = 0;
+    for(let i = 0; i < sequence.length; i++) {
+        if(sequence[i] > sequence[i+1]) {
+            strike++;
+        }
+    }
+    if(strike > 1) {
+        return false;
+    } else {
+        return true;
+    }    
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+//console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
