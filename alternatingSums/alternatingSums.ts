6e2df55a7d031e19a621bc157c65e1fc3be4a203
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-10 21:54:13 +0530

    10062020: Day 6

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 function alternatingSums(a: number[]): number[] {
+    const result: number[] = [];
+    let teamA: number = 0;
+    let teamB: number = 0;
 
+    for (let i = 0; i < a.length; i++) {
+        if (i % 2 == 0) {
+            teamA += a[i]
+        } else {
+            teamB += a[i];
+        }
+    }
+    return [teamA, teamB];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
