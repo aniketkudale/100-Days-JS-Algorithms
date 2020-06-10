71f5d21e474d115ed954ebbb7ce855042ade8b34
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-10 22:04:48 +0530

    10062020: Day 6

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,16 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    let you = 0;
+    let yourFriend = 0;
 
+    you = yourLeft + yourRight;
+    yourFriend = friendsLeft + friendsRight;
+
+    if ( you !== yourFriend) {
+        return false;
+    }
+    return true;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
