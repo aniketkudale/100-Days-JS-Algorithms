ed3f6e0f9f162e67b54baf330f506b4c0dfd94ca
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-14 20:52:47 +0530

    14062020: Day 8

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,12 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let index = 1, sum = 0;
+    for(let i=0; i<inputArray.length; i++) {
+        if (inputArray[i] != i+1) {
+             sum += index - inputArray[i]; 
+        }
+        index++;
+    }
+    return sum;
 }
 
-console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1, 1]));
