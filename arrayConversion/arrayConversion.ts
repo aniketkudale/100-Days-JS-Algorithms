6e5feed455c412ca464e37c343b6d74109b650ee
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-14 21:38:30 +0530

    14062020: Day 8

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,19 @@
 function arrayConversion(inputArray: number[]): number {
-
+    let a = inputArray, maxCount = 3, result: number[] = [];
+    for(let i=0; i<maxCount; i++) {
+        for(let j=0; j<a.length; j+=2) {
+            if ( i == 0 || i == 2) {
+                result.push( a[j] + a[j+1])
+            }
+            if ( i == 1) {
+                result.push( a[j] * a[j+1])
+            }
+        }
+        a = result;
+        result = [];
+         
+    }
+    return a[0];
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
