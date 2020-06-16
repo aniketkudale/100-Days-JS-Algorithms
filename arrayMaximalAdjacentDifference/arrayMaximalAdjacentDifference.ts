acb9ecebc8a162aa6b48d3c9fe2a730a16c098f2
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-16 20:44:58 +0530

    16062020: Day 9

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,12 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+    let max = 0, diff = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        diff = Math.abs(inputArray[i] - inputArray[i+1]);
+        if ( diff > max) {
+            max = diff;
+        }
+    }
+    return max;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
