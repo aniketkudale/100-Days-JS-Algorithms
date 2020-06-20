271d13aa4fbb7eb312e65ebdd5589d7a9ec3f17e
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-20 22:39:04 +0530

    17062020: Day 10

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,14 @@
 function arrayPreviousLess(items: number[]): number[] {
-
+    let result = [];
+    for (let i = items.length - 1; i>=0; i--) {
+        for (let j = i; j>=0; j--) {
+            if(items[i] > items[j]) {
+                result.unshift(items[j]);
+            }
+        }
+        Math.min(...items)
+    }
+    return result;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
